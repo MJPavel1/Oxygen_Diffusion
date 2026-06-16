90bfac138b866eceb0d7efbd7d57fbcc47d72d46
MJPavel1 <mjpavel@crimson.ua.edu>
2026-06-15 22:04:38 -0400

    Add data types to variables

--- src/oxygen_diffusion/materials.py
+++ src/oxygen_diffusion/materials.py
@@ -9,22 +9,22 @@ import numpy as np
 # ---------------------------
 # Universal constants
 # ---------------------------
-Rgas = 8.314  # J/mol/K
+Rgas: float = 8.314  # J/mol/K
 
 # ---------------------------
 # Dirty powder (W assumption)
 # ---------------------------
-Dd0 = 1e-7        # m^2/s
-Qd  = 150e3       # J/mol
-Csat_dirty = 1000 # wt ppm, assumed fixed for now
+Dd0: float = 1e-7        # m^2/s
+Qd: float  = 150e3       # J/mol
+Csat_dirty: float = 1000 # wt ppm, assumed fixed for now
 
 # ---------------------------
 # Scavenger (pure Ta)
 # ---------------------------
-Ds0 = np.exp(-13.72)  # m^2/s (https://doi.org/10.1016/0001-6160(86)90240-3)
-Qs  = 115.5e3          # J/mol
+Ds0: float = np.exp(-13.72)  # m^2/s (https://doi.org/10.1016/0001-6160(86)90240-3)
+Qs: float  = 115.5e3          # J/mol
 
-def tantalum_solubility(T):
+def tantalum_solubility(T: float) -> float:
     """
     Oxygen solubility in Ta (wt ppm) as a function of temperature (K).
     Unified Arrhenius fit to Ta-O phase boundary data.
